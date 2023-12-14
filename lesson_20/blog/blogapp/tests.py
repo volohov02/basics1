f667e30106add6dcbeec8b3de040e347f4f2b8bc
Александр <volohov@aaanet.ru>
2023-12-14 22:32:06 +0300

    Урок 24 тесты form и view

--- lesson_20/blog/blogapp/tests.py
+++ lesson_20/blog/blogapp/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Skills, Vacancy
+from blogapp.models import Skills, Vacancy
 from usersapp.models import BlogUser
 from mixer.backend.django import mixer
 
@@ -19,14 +19,20 @@ from mixer.backend.django import mixer
 
 class VacancyTestCase(TestCase):
 
-    # def setUp(self):
-    #     user = BlogUser.objects.create_user(username='test_user', email='test@test.com', password='leo1234567')
-    #     self.vacancy = Vacancy.objects.create(name='test_vacancy', text='some', user=user, skills=skills)
-    #
-    #     self.vacancy_str = Vacancy.objects.create(name='test_vacancy_str', text='some', user=user, skills=skills)
+    def setUp(self):
+        user = BlogUser.objects.create_user(username='test_user', email='test@test.com', password='leo1234567')
+        self.vacancy = Vacancy.objects.create(name='test_vacancy', user=user)
+        self.vacancy_str = Vacancy.objects.create(name='test_vacancy_str', user=user)
 
     def test_has_image(self):
-        user = BlogUser.objects.create_user(username='test_user', email='test@test.com', password='leo1234567')
-        vacancy = Vacancy.objects.create(name='test_vacancy', text='some', user=user)
         self.assertFalse(self.vacancy.has_image())
+
+    def test_str(self):
+        self.assertEqual(str(self.vacancy_str), 'test_vacancy_str')
+
+class SkillsTestCase(TestCase):
+
+    def test_str(self):
+        self.skills_str = Skills.objects.create(name='test_skills_str')
+        self.assertEqual(str(self.skills_str), 'test_skills_str')
 
